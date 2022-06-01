e69bec60b0a8b50c3b970ca96aa047e5aba09ef3
Paulo <git@pauloo27.dev>
2022-06-01 00:20:27 -0300

    feat(pwa): add manifest and stuff

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,9 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png" />
+        <meta name="theme-color" content="#C0FFEE" />
       </Head>
       <body>
         <Main />
