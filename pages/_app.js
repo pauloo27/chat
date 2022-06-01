5a7aa0cf578fd3e6c2e8dcfa47320dac556dc3ef
mauriciofsnts <74746082+mauriciofsnts@users.noreply.github.com>
2022-06-01 09:24:43 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <title>Click2Chat</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
       </Head>
       <Component {...pageProps} />
