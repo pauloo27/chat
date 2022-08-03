c9105a21ed46d713fa80e2bc95ece2dc52055afa
Paulo <git@pauloo27.dev>
2022-08-02 23:29:09 -0300

    feat(ios): handle "Scan Text" better

--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,19 @@ export default function Home() {
   const [number, setNumber] = React.useState('')
 
   const handleChange = (e) => {
+    // in IOS, in order to the "Scan Text" feature to work properly,
+    // we should not change the original text. insertCompositionText means
+    // "user indirectly entering text" (quote from MDN), which is emitted
+    // by scanning text. When the user "accepts" the text, another event will
+    // be triggered, any non-numbers will be removed there.
+    if (e.nativeEvent.inputType === 'insertCompositionText') return setNumber(e.target.value)
+
     const { value } = e.target
-    if (value === '') {
-      setNumber('')
-      return
-    }
+    if (value === '') return setNumber('')
 
     const numbers = value.match(/\d+/g)
-    if (numbers === null) return
-    setNumber(numbers.join(''))
+    if (numbers === null) return null
+    return setNumber(numbers.join(''))
   }
 
   return (
